7ca5514c385d2a0fe430bcc44ba3c81fb7191a77
Dima <antoshkiv1@gmail.com>
2019-04-05 13:50:07 +0000

    calc

---
+++ js/index.js
@@ -0,0 +1,26 @@
+
+const display = document.querySelector('.display');
+
+const digitsOpers = document.querySelectorAll('.digits button, .operations button');
+digitsOpers.forEach(digit => digit.addEventListener('click', digitOperPressed));
+
+
+function digitOperPressed(e) {
+    e.preventDefault();
+    display.value += e.target.innerText;
+}
+
+const equal = document.querySelector('.eq');
+equal.addEventListener('click', equalPressed);
+
+function equalPressed(e) {
+    e.preventDefault();
+    display.value = eval(display.value);
+}
+const clear = document.querySelector('.clear');
+clear.addEventListener('click', clearDisplay);
+
+function clearDisplay(e) {
+    e.preventDefault();
+    display.value = '';
+}
